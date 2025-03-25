1f767f87208724da7ad36e19005c49237ab9a408
Pablo Henrique <pablosousadev@gmail.com>
2025-03-25 17:44:28 -0300

    Test2

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
 function somar(){
     console.log("Eu sou analfabeto, meu pai é analfabeto, meu cachorro é anafalbeto, minha família toda é analfabeta");
+    console.log("Eu sou analfabeto, meu pai é analfabeto, meu cachorro é anafalbeto, minha família toda é analfabeta");
+    console.log("Eu sou analfabeto, meu pai é analfabeto, meu cachorro é anafalbeto, minha família toda é analfabeta");
+    console.log("Eu sou analfabeto, meu pai é analfabeto, meu cachorro é anafalbeto, minha família toda é analfabeta");
 }
