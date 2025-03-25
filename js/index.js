9fc3c60da1806dc57e19dd83db09a6f7ca4bf24b
João Vitor <jvpnprofissional2006@gmail.com>
2025-03-25 17:07:33 -0300

    MeuTeste

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
 function somar(){
     console.log("Eu sou analfabeto, meu pai é analfabeto, meu cachorro é anafalbeto, minha família toda é analfabeta");
+    console.log("NEGÃO");
+    console.log("NADa");
 }
